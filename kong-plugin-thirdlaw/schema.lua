90a2d7ebd0dccc1cb63928d7c50dac90e291e75e
Brian Yang <byang14@gmail.com>
2024-01-24 15:30:31 -0800

    update schema

--- kong-plugin-thirdlaw/schema.lua
+++ kong-plugin-thirdlaw/schema.lua
@@ -1,9 +1,15 @@
 return {
-    no_consumer = true,
+    name = "thirdlaw",
     fields = {
-        redirect_url = {type = "string", required = true},
-        enable_debug = {type = "boolean", default = false},
-        enable_openai_redirect = {type = "boolean", default = false},
-        enable_gemini_redirect = {type = "boolean", default = false},
-    }
+        { config = {
+            type = "record",
+            fields = {
+                { redirect_url = { type = "string", required = true } },
+                { enable_debug = { type = "boolean", default = false } },
+                { enable_openai_redirect = { type = "boolean", default = false } },
+                { enable_gemini_redirect = { type = "boolean", default = false } },
+            },
+        }},
+    },
+    no_consumer = true,
 }
